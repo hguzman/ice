5c2c4d31581dd6ebc073ed6343bd9a88f46b5205
Henry Miguel Guzmán Escorcia <hguzman10@gmail.com>
2014-05-24 14:12:16 -0500

    Script listo

--- base de datos/ice.sql
+++ base de datos/ice.sql
@@ -1,4 +1,31 @@
-USE ice;
-INSERT INTO usuarios VALUE("93111913062","123","4");
-SELECT * FROM usuarios;
-DROP 
+create database ice;
+
+use ice;
+
+create table perfiles(
+	perfil int not null AUTO_INCREMENT,
+	nombrePerfil varchar(50),
+	primary key(perfil)
+);
+
+create table usuarios(
+	usuario varchar(20),
+	contrasena varchar(20),
+	perfil int,
+	primary key (usuario),
+	FOREIGN KEY(perfil) REFERENCES perfiles(perfil)
+);
+
+
+
+insert into perfiles (nombrePerfil) value("ADMINISTRADOR");
+insert into perfiles (nombrePerfil) value("INSTITUCIÓN");
+insert into perfiles (nombrePerfil) value("DOCENTE");
+insert into perfiles (nombrePerfil) value("ESTUDIANTE");
+
+insert into usuarios value("hguzman","12345",2); 
+insert into usuarios value("luis","12345",3); 
+insert into usuarios value("102446569","123",1); 
+insert into usuarios value("1","1",4); 
+
+select * from usuarios;
